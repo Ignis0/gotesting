72342c0a796e4669afc55bc18b3db84675447407
Ignis0 <37348299+Ignis0@users.noreply.github.com>
2018-03-13 17:59:45 -0400

    Update testing.go

--- testing.go
+++ testing.go
@@ -9,25 +9,30 @@ import (
 	"encoding/json"
 )
 
+type PoliceAPI []struct {
+	District    string `json:"district,omitempty"`
+	IncDatetime string `json:"inc_datetime"`
+	IncNo       string `json:"inc_no"`
+	Lcr         string `json:"lcr"`
+	LcrDesc     string `json:"lcr_desc"`
+	Location    struct {
+		Type        string    `json:"type"`
+		Coordinates []float64 `json:"coordinates"`
+	} `json:"location,omitempty"`
+}
+
 func main() {
 	response, err := http.Get("https://data.raleighnc.gov/resource/3bhm-we7a.json")
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
-
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
+	
+	var record PoliceAPI
+	if err := json.NewDecoder(response.Body).Decode(&record); err != nil {
+		log.Println(err)
 	}
 	
-	fmt.Println(string(responseData))
-
-	entry []string `json: "inc_no"`
-	
-	for i := 0; i < len(entry); i++ {
-		fmt.Println(entry[i])
-	}
-	fmt.Println("done")
+	fmt.Println(PolieAPI.District)
 	
 }
