031b5910dcd20167be7e866711967b78d257d3e2
Ignis0 <37348299+Ignis0@users.noreply.github.com>
2018-03-13 16:34:32 -0400

    Update testing.go

--- testing.go
+++ testing.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
-type people struct {
-	Number int `json:"number"`
-}
-
 func main() {
-	text := `{"people": [{"craft": "ISS", "name": "Sergey Rizhikov"}, {"craft": "ISS", "name": "Andrey Borisenko"}, {"craft": "ISS", "name": "Shane Kimbrough"}, {"craft": "ISS", "name": "Oleg Novitskiy"}, {"craft": "ISS", "name": "Thomas Pesquet"}, {"craft": "ISS", "name": "Peggy Whitson"}], "message": "success", "number": 6}`
-	textBytes := []byte(text)
-
-	people1 := people{}
-	err := json.Unmarshal(textBytes, &people1)
+	response, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
+	
 	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Print(err.Error())
+		os.Exit(1)
 	}
-	fmt.Println(people1.Number)
+	
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
 }
